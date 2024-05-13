c227bacb6e2949ee8ee99a1c236c901e995a37ae
eyobt4 <eyobtesfaye838@gmail.com>
2024-05-13 13:07:48 +0300

    two pages added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter , Routes , Route , Link , NavLink} from 'react-router-dom'
+import Home from './pages/Home.js'
+import About from './pages/About.js'
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Jobrouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="About">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/About" element={<About/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
